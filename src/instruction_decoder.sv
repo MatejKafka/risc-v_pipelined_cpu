d7a105824a0a02750d3da8e486e989ba329ff430
Matej Kafka <git@a-n.cc>
2022-11-21 20:54:01 +0100

    instruction_decode: Implement support for all arithmetic instructions, cleanup

--- src/instruction_decoder.sv
+++ src/instruction_decoder.sv
@@ -3,35 +3,29 @@
 `include "types.sv"
 `include "alu.sv"
 
+typedef logic signed [11:00] Immediate;
 typedef struct packed {
     AluOp op;
     RegAddress rd;
     RegAddress rs1;
     Bool has_immediate;
     RegAddress rs2;
-    Word immediate;
+    Immediate immediate;
     Bool is_ebreak;
 } Instruction;
 
 task Instruction_display(Instruction i);
-    $display("%s rd=%0d rs1=%0d %0s=%0d",
-        AluOp_symbol(i.op), i.rd, i.rs1,
-        i.has_immediate ? "imm" : "rs2",
-        i.has_immediate ? $signed(i.immediate) : $signed({1'b0, i.rs2}));
+    if (i.is_ebreak) begin
+        $display("EBREAK");
+    end else begin
+        $display("%s rd=%0d rs1=%0d %0s=%0d", AluOp_symbol(i.op), i.rd, i.rs1,
+                i.has_immediate ? "imm" : "rs2",
+                i.has_immediate ? i.immediate : $signed(12'({1'b0, i.rs2})));
+    end
 endtask
 
-// iverilog does not seem to support the '{} struct initialization syntax, so we'll have to do it manually
-`define INSTRUCTION(I, OP, RD, RS1, HAS_IMMEDIATE, RS2, IMM_SIGN, IMM, IS_EBREAK) do begin \
-        I.op = OP; \
-        I.rd = RD; \
-        I.rs1 = RS1; \
-        I.has_immediate = HAS_IMMEDIATE; \
-        I.rs2 = RS2; \
-        I.immediate = {{21{IMM_SIGN}}, IMM}; \
-        I.is_ebreak = IS_EBREAK; \
-    end while (0)
 
-/** Note that the instruction decoder uses fixed type sizes, unlike the rest of the project. */
+// Note that the instruction decoder uses fixed type sizes, unlike the rest of the project.
 module instruction_decoder(input Word in, output Instruction out);
     // read the opcode, call the corresponding function to handle that type of instructions
     // since not all opcodes have a standard name, some of the names are made up (like IA = "immediate arithmetic")
@@ -43,29 +37,36 @@ module instruction_decoder(input Word in, output Instruction out);
     endcase
 
 
-    // ADD & co
+    // R-type instructions (register-to-register)
     function void decode_R(logic [6:0] funct7, RegAddress rs2, rs1, logic [2:0] funct3, RegAddress rd);
-        AluOp op;
-        case ({funct7, funct3})
-            'b0000000_000: op = ADD;
-            'b0100000_000: op = SUB;
-            // SLL
-            // SLT
-            // SLU
-            'b0000000_100: op = XOR;
-            // SRL
-            // SRA
-            'b0000000_110: op = OR;
-            'b0000000_111: op = AND;
-            default: panic("Invalid/unsupported R instruction.");
-        endcase
-        `INSTRUCTION(out, op, rd, rs1, FALSE, rs2, 1'b0, 1'b0, FALSE);
+        // AluOps are encoded the same way as in the instruction, so we just combine the relevant bits
+        automatic AluOp op = AluOp'({funct7[5], funct3});
+        // validate the instruction
+        if ({funct7[6], funct7[4:0]} != 0) panic("Invalid/unsupported R instruction, unknown 'funct7' value.");
+        if (funct7[5] && funct3 != 3'b000 && funct3 != 3'b101) panic("Invalid/unsupported R instruction, unknown 'funct7'/'funct3' value combination.");
+        out = '{op, rd, rs1, FALSE, rs2, '0, FALSE};
     endfunction
 
 
-    // ADDI & co
-    function void decode_IA(logic [11:0] imm, RegAddress rs1, logic [2:0] funct3, RegAddress rd);
-        `INSTRUCTION(out, ADD, rd, rs1, TRUE, 5'b0, imm[11], imm[10:0], FALSE);
+    // Immediate Arithmetic instructions
+    function void decode_IA(Immediate imm, RegAddress rs1, logic [2:0] funct3, RegAddress rd);
+        automatic AluOp op;
+        automatic Immediate imm_resolved;
+
+        if (funct3 == 3'b?01) begin
+            // shifts have 'funct7' and a shorter immediate
+            if (funct3 == 3'b001) begin
+                if (imm[11:5] != 0) panic("Invalid/unsupported IA shift instruction, unknown 'funct7' value.");
+            end else begin
+                if ({imm[11], imm[9:5]} != 0) panic("Invalid/unsupported IA shift instruction, unknown 'funct7' value.");
+            end
+            op = AluOp'({imm[10], 3'b101});
+            imm_resolved = Immediate'($unsigned(imm[4:0]));
+        end else begin
+            op = AluOp'({1'b0, funct3});
+            imm_resolved = imm;
+        end
+        out = '{op, rd, rs1, TRUE, '0, imm_resolved, FALSE};
     endfunction
 
 
@@ -74,11 +75,12 @@ module instruction_decoder(input Word in, output Instruction out);
         if (zeros != 0) panic("Invalid/unsupported E instruction.");
         case (type_)
             12'b000000000000: panic("ECALL not supported.");
-            12'b000000000001: `INSTRUCTION(out, ADD, RegAddress'(0), RegAddress'(0), FALSE, RegAddress'(0), 1'b0, 11'b0, TRUE);
+            12'b000000000001: begin out = '0; out.is_ebreak = TRUE; end
             default: panic("Invalid/unsupported E instruction.");
         endcase
     endfunction
 endmodule
+
 
 `ifdef TEST_instruction_decoder
 `include "instruction_macros.sv"
