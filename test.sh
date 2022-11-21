73509e9ef56a2b1e0bc33101305ce73df198810d
Matej Kafka <git@a-n.cc>
2022-11-21 20:48:59 +0100

    Switch to verilator compilation instead of iverilog
    IVerilog support for SystemVerilog is quite bad, and the error messages are truly sad. Verilator seems quite a bit better.

--- test.sh
+++ test.sh
@@ -18,17 +18,27 @@ build_dir="$(realpath $(dirname $BASH_SOURCE)/build)"
 src_dir="$(realpath $(dirname $BASH_SOURCE)/src)"
 
 tb_enabler="TEST_$(basename "$target_rel")"
-target="$build_dir/$target_rel.vvp"
+target="$build_dir/$(dirname "$target_rel")/V$(basename "$target_rel")"
 target_output="$build_dir/$target_rel.vcd"
 target_dir="$(dirname "$target")"
 src="$src_dir/$target_rel.sv"
 
-mkdir -p "$(dirname "$target")"
-# run slang in --lint-only mode to get better error messages
+mkdir -p "$target_dir"
+
+# run slang (from Windows) in --lint-only mode to get better error messages
 slang.exe --lint-only -Wextra -D"$tb_enabler" "${debug[@]}" -I"$(wslpath -w "$src_dir")" "$(wslpath -w "$src")" --quiet
-iverilog -g2012 -Wall -t vvp -D"$tb_enabler" "${debug[@]}" -I"$src_dir" -o "$target" "$src"
+# compile to an executable
+verilator \
+    --binary --trace --build-jobs 0 -MAKEFLAGS "-s OPT_FAST=-O0 CXX=/usr/lib/ccache/gcc CXX=/usr/lib/ccache/g++" -CFLAGS "-fuse-ld=mold" \
+    -Wall -Wpedantic -Wno-EOFNEWLINE -Wno-DECLFILENAME \
+    --x-assign 1 --x-initial unique \
+    --top-module "$(basename "$target_rel")_tb" -D"$tb_enabler" "${debug[@]}" \
+    -I"$src_dir" --Mdir "$target_dir" -o "$target" "$src" \
+    >/dev/null # hide build prints
+
 cd "$target_dir"
-vvp "$target"
+# run the simulation
+"$target"
 
 if [[ "$visualize" == "1" ]]; then
     gtkwave.exe "$(realpath --relative-to=. "$target_output")" 1>/dev/null 2>/dev/null
