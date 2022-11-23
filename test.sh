5891741211be26f32aaad0fa96ad9d7b2ac33c6b
Matej Kafka <git@a-n.cc>
2022-11-23 04:35:25 +0100

    test.sh: Show more warnings, randomize initial variable values

--- test.sh
+++ test.sh
@@ -26,19 +26,23 @@ src="$src_dir/$target_rel.sv"
 mkdir -p "$target_dir"
 
 # run slang (from Windows) in --lint-only mode to get better error messages
-slang.exe --lint-only -Wextra -D"$tb_enabler" "${debug[@]}" -I"$(wslpath -w "$src_dir")" "$(wslpath -w "$src")" --quiet
+slang.exe --lint-only --quiet \
+    -Wextra -Wpedantic -Wconversion \
+    -D"$tb_enabler" "${debug[@]}" -I"$(wslpath -w "$src_dir")" "$(wslpath -w "$src")"
+
 # compile to an executable
 verilator \
     --binary --trace --build-jobs 0 -MAKEFLAGS "-s OPT_FAST=-O0 CXX=/usr/lib/ccache/gcc CXX=/usr/lib/ccache/g++" -CFLAGS "-fuse-ld=mold" \
-    -Wall -Wpedantic -Wno-EOFNEWLINE -Wno-DECLFILENAME \
+    -Wall -Wpedantic -Wno-EOFNEWLINE -Wno-DECLFILENAME -Wno-UNUSEDSIGNAL \
     --x-assign 1 --x-initial unique \
     --top-module "$(basename "$target_rel")_tb" -D"$tb_enabler" "${debug[@]}" \
     -I"$src_dir" --Mdir "$target_dir" -o "$target" "$src" \
     >/dev/null # hide build prints
 
 cd "$target_dir"
-# run the simulation
-"$target"
+# run the simulation, set a seed for the initial values of uninitialized variables to catch incorrect resets
+# https://verilator.org/guide/latest/exe_sim.html
+"$target" +verilator+rand+reset+1
 
 if [[ "$visualize" == "1" ]]; then
     gtkwave.exe "$(realpath --relative-to=. "$target_output")" 1>/dev/null 2>/dev/null
